f19ee9f7fd6c2ac442cfc53483b3a73c7332663f
Ahmet Taha Tosun <126511572+alyxville@users.noreply.github.com>
2023-03-05 14:46:50 +0300

    asdasdasd

--- server/rolecontrol.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '../server/baglan.php';
-$uid = $_SESSION['id'];
-$query = mysqli_query($conn, "SELECT * FROM `sh_kullanici` WHERE id='$uid'");
-while ($getvar = mysqli_fetch_assoc($query)) {
-    $k_rol = $getvar['k_rol'];
-}
